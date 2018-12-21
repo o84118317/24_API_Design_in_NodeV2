6591081fdcf92a770010863c1f6ac34e8dd8721d
o84118317 <o84118317@gmail.com>
2018-12-22 02:11:59 +0800

    set up express app

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import http from 'http'
 import { createServer } from 'http'
 
 import app from './server'
-let currentApp = app
 
+app.listen(3000, () => {
+	console.log('API on port 3000')
+})
+
+let currentApp = app
 
 if (module.hot) {
 	module.hot.accept(['./server'], () => {
