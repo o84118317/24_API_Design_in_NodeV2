4b496349ae97907a2db102c4b681097ef111ea10
o84118317 <o84118317@gmail.com>
2018-12-22 02:03:41 +0800

    first commit

---
+++ src/server.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import setupMiddware from './middleware'
+import { restRouter } from './api'
+import { connect } from './db'
+import { signin, protect } from './api/modules/auth'
+// Declare an app from express
+const app = express()
+
+app.get('', (req, res)=> {
+	res.json({ok: true})
+})
+
+
+
+
+export default app
